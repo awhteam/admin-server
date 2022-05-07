ce805326a3021409049185231bc82945ff017525
darkido <darkido256@gmail.com>
2022-05-07 19:00:46 +0430

    [feat]: add watch command

--- SxF_mod/handlers.py
+++ SxF_mod/handlers.py
@@ -6,8 +6,19 @@ import regex
 import traceback
 import os
 
+WATCH_MESSAGES=[]
 
 
 @Bot.on_message(filters.command(["start"]))
 async def start(bot: Bot, message: Message):
     await message.reply(text="بات زندس", quote=True)
+
+
+
+@Bot.on_message(filters.command(["watch"]))
+async def watch_command(bot: Bot, message: Message):
+    chat="Latest_Ongoing_Airing_Anime"
+    postId= int(message.command[1].split("Latest_Ongoing_Airing_Anime/")[-1])
+    await message.reply(text=postId, quote=True)
+    WATCH_MESSAGES.append(postId)
+
